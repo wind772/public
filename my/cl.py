18f63fe16d47299c7ab3f932b5fbc165afa7bac5
wind772 <wind772@gmail.com>
2015-04-10 00:41:01 +0900

    cl

--- my/cl.py
+++ my/cl.py
@@ -193,8 +193,8 @@ def rotatematrix(data):
         newdata.append(newrow)
     return newdata
 
-def main():
-     a, b, c = readfile('blogdata.txt')
+def testhcluster():
+     blognames, words, data = readfile('blogdata.txt')
      clust = hcluster(c)
      printclust(clust, labels=a)
      drawdendrogram(clust, a, jpeg='blostclust.jpg')
@@ -202,6 +202,52 @@ def main():
      rdata = rotatematrix(c)
      wordclust = hcluster(rdata)
      drawdendrogram(wordclust, labels=b, jpeg='wordclust.jpg')
+
+
+import random
+
+def kcluster(rows, distance=pearson, k=4):
+    ranges=[(min([row[i] for row in rows]), max([row[i] for row in rows]))
+                for i in range(len(rows[0]))]
+  
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0]
+                for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches=None
+    for t in range(100):
+        print 'Iteration %d' % t
+        bestmatches=[[] for i in range(k)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch=0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch],row): 
+                    bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches == lastmatches:
+            break
+
+        lastmatches = bestmatches
+
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+
+        return bestmatches
+
+def main():
+     blognames, words, data = readfile('blogdata.txt')
+     clust =  kcluster(data)
+     print [blognames[r] for r in clust[0]]
 
 if __name__ == "__main__":
     main()
